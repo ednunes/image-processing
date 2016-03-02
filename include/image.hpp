56b474fe67214589907fc2e5c0c534221ea6acb8
Eduardo_Nunes <eduardonunes2525@gmail.com>
2016-03-02 11:00:45 -0300

    Add attributes in class image

---
+++ include/image.hpp
@@ -0,0 +1,37 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+
+class Image
+{
+private:
+	// Type that defines the image
+	// Value contained in the header, in first position
+	std::string typeImage;
+	// Number of columns in image
+	// Value contained in the header, in second position
+	int numberColumnsHeader;
+	// Number of lines in image
+	// Value contained in the header, in third posistion
+	int numberLinesHeader;
+	// Number of max level in image
+	// Value contained in the header in fourth position
+	int maxLevelGray;
+	// Number of columns in image file
+	// Value to be determined by mapping
+	int* numberElementsColumnsImageFile;
+	// Number of lines in image
+	// Value to be determined by mapping
+	int numberLinesImageFile;
+	 
+	
+public:
+	Image();
+	//~Image();
+
+
+	
+};
